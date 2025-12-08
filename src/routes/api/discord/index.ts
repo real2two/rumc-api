9d6c5b679ac629ee781279cad32aa47bb4ac0d63
real2two <26689496+real2two@users.noreply.github.com>
2025-12-08 11:01:09 -0500

    fix: remove unnecessary tags

--- src/routes/api/discord/index.ts
+++ src/routes/api/discord/index.ts
@@ -1,10 +1,7 @@
 import Elysia from "elysia";
 import { handler } from "~/discord";
 
-export const discordRoutes = new Elysia({
-	prefix: "/discord",
-	tags: ["Discord"],
-})
+export const discordRoutes = new Elysia({ prefix: "/discord" })
 	// Handle Carbon routes
 	.all("/*", ({ request }) => handler(request, {}), {
 		detail: { hide: true },
