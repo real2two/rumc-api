e248153a586889a33bb6bad911c621268f90fcdd
real2two <26689496+real2two@users.noreply.github.com>
2025-12-15 17:25:23 -0500

    fix: remove security in server in openapi docs

--- src/routes/api/servers/index.ts
+++ src/routes/api/servers/index.ts
@@ -10,60 +10,66 @@ const SERVER_ADDRESSES = {
 export const serversRoutes = new Elysia({
 	prefix: "/servers",
 	tags: ["Servers"],
-}).get(
-	"/:id",
-	async ({ params, set }) => {
-		try {
-			// Get server address based on the ID
-			const serverAddress = SERVER_ADDRESSES[params.id];
+})
+	// Get server
+	.get(
+		"/:id",
+		async ({ params, set }) => {
+			try {
+				// Get server address based on the ID
+				const serverAddress = SERVER_ADDRESSES[params.id];
 
-			// Get cached server
-			const cachedServer = await getServerCache(params.id);
-			if (cachedServer) return cachedServer;
+				// Get cached server
+				const cachedServer = await getServerCache(params.id);
+				if (cachedServer) return cachedServer;
 
-			// If not cached, fetch server
-			const res = await fetch(
-				`https://api.mcstatus.io/v2/status/java/${serverAddress}`,
-			);
-			if (!res.ok) throw new Error("Failed to fetch server");
+				// If not cached, fetch server
+				const res = await fetch(
+					`https://api.mcstatus.io/v2/status/java/${serverAddress}`,
+				);
+				if (!res.ok) throw new Error("Failed to fetch server");
 
-			// Get response
-			const data = (await res.json()) as {
-				online: boolean;
-				players?: { online?: number };
-			};
+				// Get response
+				const data = (await res.json()) as {
+					online: boolean;
+					players?: { online?: number };
+				};
 
-			// Cache server data
-			const server = {
-				online: data.online,
-				players: data.players?.online || 0,
-			};
-			await setServerCache(
-				params.id,
-				server,
-				Number.parseInt(res.headers.get("X-Cache-Time-Remaining") || "60", 10),
-			);
+				// Cache server data
+				const server = {
+					online: data.online,
+					players: data.players?.online || 0,
+				};
+				await setServerCache(
+					params.id,
+					server,
+					Number.parseInt(
+						res.headers.get("X-Cache-Time-Remaining") || "60",
+						10,
+					),
+				);
 
-			// Send response
-			return server;
-		} catch (err) {
-			console.error("Error fetching player count:", err);
+				// Send response
+				return server;
+			} catch (err) {
+				console.error("Error fetching player count:", err);
 
-			set.status = 500;
-			return { error: ErrorCodes.InternalServerError };
-		}
-	},
-	{
-		detail: {
-			summary: "Get server status",
-			description: "Get online status and player count (cached)",
+				set.status = 500;
+				return { error: ErrorCodes.InternalServerError };
+			}
 		},
-		params: t.Object({
-			id: t.Union([t.Literal("survival"), t.Literal("limbo")]),
-		}),
-		response: {
-			200: t.Object({ online: t.Boolean(), players: t.Number() }),
-			500: t.Object({ error: t.Literal(ErrorCodes.InternalServerError) }),
+		{
+			detail: {
+				summary: "Get server status",
+				description: "Get online status and player count (cached)",
+				security: [],
+			},
+			params: t.Object({
+				id: t.Union([t.Literal("survival"), t.Literal("limbo")]),
+			}),
+			response: {
+				200: t.Object({ online: t.Boolean(), players: t.Number() }),
+				500: t.Object({ error: t.Literal(ErrorCodes.InternalServerError) }),
+			},
 		},
-	},
-);
+	);
