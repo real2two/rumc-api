af9bcc2961af664bbc42f7eb7ff5b06bdd019f17
Two <26689496+real2two@users.noreply.github.com>
2026-01-11 13:05:11 -0500

    Update regex.ts

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,6 +1,6 @@
 export const Regex = {
 	Uuid: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
-	ScarletMail: /^[a-z][a-z0-9_+-]+@scarletmail\.rutgers\.edu$/,
+	ScarletMail: /^[a-z][a-z0-9]+@scarletmail\.rutgers\.edu$/,
 	MinecraftUsername: /^[a-zA-Z0-9_]{1,16}$/,
 };
 
