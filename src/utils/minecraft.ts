040977262d99d83b1c6f5976ce089deaccc6db30
real2two <26689496+real2two@users.noreply.github.com>
2026-02-25 16:12:27 -0500

    fix: don't crash on fetch playerdb and fails

--- src/utils/minecraft.ts
+++ src/utils/minecraft.ts
@@ -6,18 +6,23 @@
 export async function getMinecraftPlayer(
 	username: string,
 ): Promise<{ id: string; username: string } | null> {
-	const res = await fetch(
-		`https://playerdb.co/api/player/minecraft/${encodeURIComponent(username)}`,
-		{ headers: { "User-Agent": "RUMC/1.0 (rumc.club)" } },
-	);
-	if (!res.ok) return null;
+	try {
+		const res = await fetch(
+			`https://playerdb.co/api/player/minecraft/${encodeURIComponent(username)}`,
+			{ headers: { "User-Agent": "RUMC/1.0 (rumc.club)" } },
+		);
+		if (!res.ok) return null;
 
-	const json = await res.json();
-	const player = {
-		// @ts-expect-error This is correct
-		id: json?.data?.player?.id,
-		// @ts-expect-error This is correct
-		username: json?.data?.player?.username,
-	};
-	return player.id && player.username ? player : null;
+		const json = await res.json();
+		const player = {
+			// @ts-expect-error This is correct
+			id: json?.data?.player?.id,
+			// @ts-expect-error This is correct
+			username: json?.data?.player?.username,
+		};
+		return player.id && player.username ? player : null;
+	} catch (err) {
+		console.error("Error on getting Minecraft player:", err);
+		return null;
+	}
 }
